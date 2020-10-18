5b15473fac4921c9b4ad4fa99f60bf64a90d88bb
Viktor Tishchenko <web22pro@gmail.com>
2020-10-18 22:41:44 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let st = str;
+    let br = bracketsConfig.map((el) => el.join(""));
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < br.length; j++) {
+            st = st.replace(br[j], "");
+        }
+        if (!st) return true;
+    }
+    return false;
+};
